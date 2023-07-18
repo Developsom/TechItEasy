2be334fe6d9abc118e91ec67b4982bc5f6b86271
Developsom <100343391+Developsom@users.noreply.github.com>
2023-07-18 14:10:19 +0200

    Added annotations

--- src/main/java/com/example/techiteasy/TelevisionController.java
+++ src/main/java/com/example/techiteasy/TelevisionController.java
@@ -1,4 +1,19 @@
 package com.example.techiteasy;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class TelevisionController {
+    @GetMapping("television");
+
+    @GetMapping("television/{id}");
+
+    @PutMapping("television");
+
+    @PostMapping("television");
+
+    @DeleteMapping("television");
+
+
 }
